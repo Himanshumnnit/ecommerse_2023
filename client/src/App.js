43452e4edfea121c698b248e884469e25d17d77d
Dinesh <dineshkumarpaliwal83@gmail.com>
2023-10-22 03:04:53 +0530

    Basic design made for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+        <Routes>
+             <Route path="/" element={<Homepage/>}/>
+             <Route path="/about" element={<About/>}/>
+             <Route path="/contact" element={<Contact/>}/>
+             <Route path="/policy" element={<Policy/>}/>
+             <Route path="/*" element={<Pagenotfound/>}/>
+        </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React, { children } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <main style={{ minHeight: "70.8vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
